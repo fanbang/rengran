901d41ae52733165ecdd091027d1bb107a9c0df5
fanbang <43340523+fanbang@users.noreply.github.com>
2024-11-15 09:13:26 +0800

    Update yum8.sh

--- yum8.sh
+++ yum8.sh
@@ -4,10 +4,10 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 #备份
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-#阿里云源
+####阿里云源
 curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-vault-8.5.2111.repo
-#更新缓存
+####更新缓存
 yum clean all
 yum makecache
-#更新系统
+#####更新系统
 yum update 

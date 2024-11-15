bd5d855d9a3550867003ed85bb7a31ff642bef54
fanbang <43340523+fanbang@users.noreply.github.com>
2024-11-15 09:19:43 +0800

    Update yum8.sh

--- yum8.sh
+++ yum8.sh
@@ -1,15 +1,2 @@
-#!/bin/sh
-####将Centos 8的yum源更换为国内的阿里云源###
-cd /etc/yum.repos.d/
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-####备份####
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-####1####
-####阿里云源####
-curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-vault-8.5.2111.repo
-####更新缓存####
-yum clean all
-yum makecache
-#####更新系统####
-yum update 
+#!/usr/bin/env bash
+echo "sss"

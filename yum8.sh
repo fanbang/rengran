3359a3d78e18b01f2f13a77d686dfb94f3e24375
fanbang <43340523+fanbang@users.noreply.github.com>
2025-01-25 20:44:37 +0800

    Update yum8.sh

--- yum8.sh
+++ yum8.sh
@@ -1,41 +1,17 @@
-#!/usr/bin/env bash
-exec 6<> /dev/tcp/localhost/80
-if [ $? -eq 0 ]; then
-    echo "80 端口可以使用"
-    exec 6<&-  # 关闭输入
-    exec 6>&-  # 关闭输出
-else
-    echo "80 端口无法使用"
+#!/bin/bash
+
+# 检查是否提供了端口参数
+if [ "$#" -eq 0 ]; then
+    echo "请提供至少一个端口作为参数。"
+    exit 1
 fi
-exec 6<> /dev/tcp/localhost/465
-if [ $? -eq 0 ]; then
-    echo "465 端口可以使用"
-    exec 6<&-  # 关闭输入
-    exec 6>&-  # 关闭输出
-else
-    echo "465 端口无法使用"
-fi
-exec 6<> /dev/tcp/localhost/143
-if [ $? -eq 0 ]; then
-    echo "143 端口可以使用"
-    exec 6<&-  # 关闭输入
-    exec 6>&-  # 关闭输出
-else
-    echo "143 端口无法使用"
-fi
-exec 6<> /dev/tcp/localhost/587
-if [ $? -eq 0 ]; then
-    echo "587 端口可以使用"
-    exec 6<&-  # 关闭输入
-    exec 6>&-  # 关闭输出
-else
-    echo "587 端口无法使用"
-fi
-exec 6<> /dev/tcp/localhost/25
-if [ $? -eq 0 ]; then
-    echo "25 端口可以使用"
-    exec 6<&-  # 关闭输入
-    exec 6>&-  # 关闭输出
-else
-    echo "25 端口无法使用"
-fi
+
+# 遍历所有提供的端口参数
+for port in "$@"; do
+    # 使用netcat监听指定的端口
+    echo "正在监听端口: $port"
+    nc -l "$port" &
+done
+
+# 等待所有后台进程完成
+wait

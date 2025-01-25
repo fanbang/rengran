d933b62ea9ba8d2d88835e55422d515486f9e5d6
fanbang <43340523+fanbang@users.noreply.github.com>
2025-01-25 20:05:03 +0800

    Update yum8.sh

--- yum8.sh
+++ yum8.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-echo "sss"
+exec 6<> /dev/tcp/localhost/25
+if [ $? -eq 0 ]; then
+    echo "25 端口可以使用"
+    exec 6<&-  # 关闭输入
+    exec 6>&-  # 关闭输出
+else
+    echo "25 端口无法使用"
+fi

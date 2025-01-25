589f11436c0936d63490b57c29102a3b316024c2
fanbang <43340523+fanbang@users.noreply.github.com>
2025-01-25 20:13:59 +0800

    Update yum8.sh

--- yum8.sh
+++ yum8.sh
@@ -7,3 +7,28 @@ if [ $? -eq 0 ]; then
 else
     echo "25 端口无法使用"
 fi
+
+exec 6<> /dev/tcp/localhost/465
+if [ $? -eq 0 ]; then
+    echo "465 端口可以使用"
+    exec 6<&-  # 关闭输入
+    exec 6>&-  # 关闭输出
+else
+    echo "465 端口无法使用"
+fi
+exec 6<> /dev/tcp/localhost/143
+if [ $? -eq 0 ]; then
+    echo "143 端口可以使用"
+    exec 6<&-  # 关闭输入
+    exec 6>&-  # 关闭输出
+else
+    echo "143 端口无法使用"
+fi
+exec 6<> /dev/tcp/localhost/587
+if [ $? -eq 0 ]; then
+    echo "587 端口可以使用"
+    exec 6<&-  # 关闭输入
+    exec 6>&-  # 关闭输出
+else
+    echo "587 端口无法使用"
+fi
